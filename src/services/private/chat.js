1dd1d88b30261f9f488be4e3615152bb4a1d1007
Nadirte <nadirhussain.te@gmail.com>
2025-08-12 01:27:57 +0500

    Client account tested

--- src/services/private/chat.js
+++ src/services/private/chat.js
@@ -4,7 +4,7 @@ export const authApi = privateAPI.injectEndpoints({
   endpoints: build => ({
     createChat: build.mutation({
         query: body => ({
-          url: '/instagram/chat-message/',
+          url: '/instagram/generate-carousel/',
           method: 'POST',
           body
         }),
